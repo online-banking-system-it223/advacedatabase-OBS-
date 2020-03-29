6d6348214eca25458e8e04ed9b3f88b90c64c9d3
Johncs2d <34950240+Johncs2d@users.noreply.github.com>
2020-03-29 15:26:21 +0800

    Update settings.py

--- dnscOBS/settings.py
+++ dnscOBS/settings.py
@@ -174,15 +174,16 @@ STATICFILES_DIRS = (
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   [os.path.join(BASE_DIR, 'static')]
-# STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-# # Extra lookup directories for collectstatic to find static files
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
+#  Add configuration for static files storage using whitenoise
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
