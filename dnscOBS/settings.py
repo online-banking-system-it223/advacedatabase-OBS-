b8bfdcaa72195fa6f6480df8cfc54bb4e7977441
John Albert <34950240+Johncs2d@users.noreply.github.com>
2020-06-26 13:21:01 +0800

    update

--- dnscOBS/settings.py
+++ dnscOBS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7d^c&&ag94n8y2#@y$ckv-yhs^%ylhl^z@+$#(6a-zocr&zh#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','bank-of-dnsc.herokuapp.com']
 
 AUTH_USER_MODEL = 'banking.MyUser'
 
